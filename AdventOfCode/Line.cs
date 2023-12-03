e60972f3838f6e75f70750e68bd84e114cd20a13
Pete Dring <dringp@fulford.york.sch.uk>
2023-12-03 08:10:29 +0000

    Made template

---
+++ AdventOfCode/Line.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    internal class Line
+    {
+        public string Definition;
+        public static List<Line> ReadLines(string path)
+        {
+            string[] strings = File.ReadAllLines(path);
+            List<Line> lines = new List<Line>();
+            foreach(string s in strings)
+            {
+                lines.Add(new Line(s));
+            }
+            return lines;
+        }
+
+        public Line(string line)
+        {
+            Definition = line;
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -6,23 +6,13 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            // Day 1
-            string[] lines = File.ReadAllLines("PuzzleInput.txt");
+            List<Line> lines = Line.ReadLines("PuzzleInput.txt");
 
             int sum = 0;
 
-            foreach(string line in lines)
+            foreach(Line line in lines)
             {
-                // find first digit
-                string firstDigit = Regex.Match(line, @"^.*?(\d)").Groups[1].Value;
-
-                // find last digit
-                string lastDigit = Regex.Match(line, @".*(\d)").Groups[1].Value;
-
-                Console.WriteLine($"Trying to calculate the calibration value for {line}: {firstDigit} {lastDigit}");
-
-                // concatenate and convert to integer
-                sum += int.Parse(firstDigit + lastDigit);
+                
             }
 
             // display the sum
