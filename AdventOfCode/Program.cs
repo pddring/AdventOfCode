fa56dd1b55271fcb98db5f198b72486899e2a854
Pete Dring <dringp@fulford.york.sch.uk>
2023-12-04 11:06:38 +0000

    Part 2

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -4,14 +4,20 @@ namespace AdventOfCode
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
             List<Line> lines = Line.ReadLines("PuzzleInput.txt");
+            Dictionary<int, int> cardCount = new Dictionary<int, int>();
+            
 
             int sum = 0;
 
-            foreach(Line line in lines)
+            /*
+            Part 1
+            foreach (Line line in lines)
             {
+                
                 int winners = 0;
                 foreach(int n in line.winningNumbers)
                 {
@@ -23,6 +29,36 @@ namespace AdventOfCode
                 int score = (int)Math.Pow(2, winners - 1);
                 Console.WriteLine($"Game {line.GameID} is worth {score} points ({winners} cards match)");
                 sum += score;
+
+            }
+            */
+
+            foreach(Line line in lines)
+            {
+                cardCount[line.GameID] = 1;
+            }
+
+            foreach (Line line in lines)
+            {
+                int winners = 0;
+                foreach (int n in line.winningNumbers)
+                {
+                    if (line.yourNumbers.Contains(n))
+                    {
+                        winners++;
+                    }
+                }
+                Console.WriteLine($"Card {line.GameID} has {winners} winners");
+                for(int i = line.GameID + 1; i <= line.GameID + winners; i++)
+                {
+                    Console.WriteLine($"So you win a copy of {i}");
+                    cardCount[i] += cardCount[line.GameID];
+                }
+            }
+
+            foreach(int i in cardCount.Keys) {
+                Console.WriteLine($"You have a total of {cardCount[i]} copies of card {i}");
+                sum += cardCount[i];
             }
 
             // display the sum
